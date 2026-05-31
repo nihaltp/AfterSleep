5e15bb3c80e777fb9edcdd88ae2c5c9097928491
NIHAL  T P <nihaltpnki@gmail.com>
2026-06-01 02:12:06 +0530

    Add ABI splits and version code mapping for multiple architectures

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,15 @@ android {
         }
     }
 
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = true
+        }
+    }
+
     signingConfigs {
         create("release") {
             storeFile = file(keystoreProperties["storeFile"] as String)
@@ -103,4 +112,25 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
     testImplementation("io.mockk:mockk:1.13.11")
+}
+
+// https://stackoverflow.com/questions/70917287/how-to-assign-different-versioncode-for-multiple-architecture-apks-built-with-fl/70942242#70942242
+// Map each ABI to a unique integer suffix
+val abiCodes = mapOf(
+    "armeabi-v7a" to 1,
+    "arm64-v8a" to 2,
+    "x86" to 3,
+    "x86_64" to 4
+)
+
+android.applicationVariants.all {
+    val variant = this
+    variant.outputs.all {
+        val output = this as? com.android.build.gradle.api.ApkVariantOutput
+        val abi = output?.getFilter(com.android.build.VariantOutput.FilterType.ABI)
+        if (abi != null) {
+            val baseAbiVersionCode = abiCodes[abi] ?: 0
+            output.versionCodeOverride = variant.versionCode * 1000 + baseAbiVersionCode
+        }
+    }
 }
