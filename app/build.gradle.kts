6ad1978eaa644e3ed187d0bc9f6000c3d259d124
NIHAL  T P <nihaltpnki@gmail.com>
2026-06-07 12:32:34 +0530

    chore: enable apk size shrinking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,8 @@ android {
             resValue("string", "app_name", "AfterSleep Debug")
         }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
