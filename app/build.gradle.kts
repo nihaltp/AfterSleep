88730b051cc928203987c46bff9e5c360cecabc8
NIHAL  T P <nihaltpnki@gmail.com>
2026-08-16 00:43:09 +0530

    chore: update Kotlin options to use new DSL for JVM target

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@ if (keystorePropertiesFile.exists()) {
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("org.jlleitschuh.gradle.ktlint")
 }
 
@@ -69,18 +70,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 
     buildFeatures {
         compose = true
         buildConfig = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
     }
 
     packaging {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application") version "8.5.2" apply false
     id("org.jetbrains.kotlin.android") version "2.4.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1" apply false
 }
 
